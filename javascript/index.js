9e410fb9288903df62695aa656cd201071fb5e50
jesscov12 <110857920+jesscov12@users.noreply.github.com>
2024-07-25 17:18:48 -0500

    added src code for timezone

--- javascript/index.js
+++ javascript/index.js
@@ -2,4 +2,6 @@ let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = " today time";
+losAngelesTimeElement.innerHTML = moment().format(
+  "h:mm:ss[<small>] A[</small>]"
+);
